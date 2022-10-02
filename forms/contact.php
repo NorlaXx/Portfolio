7b06661f61752ab13a9ceaf05e05390c2cc0d3d6
Alexis Rochefort <alexis.rochefort35@gmail.com>
2022-10-02 17:33:29 +0200

    modify the contact-form

--- forms/contact.php
+++ forms/contact.php
@@ -1,11 +1,6 @@
 <?php
-  
-  //Le mail sera envoyé à cette addresse
-  $receiving_email_address = 'alexis.rochefort35@gmail.com';
-
-  //Si le bouton "envoyé" a été appuyé
-  if(isset($_POST["submit"])){
-    $sendMail = mail($receiving_email_address, $_POST["subject"], $_POST["message"], "From:contact@portfoliorochefort.com" . "\r\n" . "Reply-to:" . $_POST["email"]);
+  if(isset($_POST["message"])){
+    $sendMail = mail("alexis.rochefort35@gmail.com", $_POST["subject"], $_POST["message"], "From:contact@portfoliorochefort.com" . "\r\n" . "Reply-to:" . $_POST["email"]);
     //Si l'email a été envoyé
     if($sendMail){
       echo("<p>Votre message a été envoyé, Merci !</p>");
